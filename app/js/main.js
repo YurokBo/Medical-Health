f07c7742893e05f4f5f44cf71da2034ed7ba0ae9
YurokBo <vertel3012@mail.ru>
2020-06-02 21:58:42 +0300

    main page finished

--- app/js/main.js
+++ app/js/main.js
@@ -4,3 +4,28 @@ $(function () {
 });
 
 
+let swiper = new Swiper('.swiper-container', {
+    slidesPerView: 5,
+    loop: true,
+    freeModeMomentum: true,
+    autoplay: {
+        delay: 10000,
+    },
+    direction: getDirection(),
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    on: {
+        resize: function () {
+            swiper.changeDirection(getDirection());
+        }
+    }
+});
+
+function getDirection() {
+    let windowWidth = window.innerWidth;
+    let direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+    return direction;
+}
